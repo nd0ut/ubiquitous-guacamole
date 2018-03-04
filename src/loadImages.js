506350eb00d69a0dc787cbecd04e8a8da4ecc679
Александр Гренишин <nd0ut.me@gmail.com>
2018-03-04 18:50:17 +0300

    refactor main function

--- src/loadImages.js
+++ src/loadImages.js
@@ -1,4 +1,6 @@
-type ReturnItem = [HTMLImageElement, string];
+type State = 'loaded' | 'failed';
+type ReturnItem = [HTMLImageElement, State];
+
 
 function normalizeInput(input: Array<string | HTMLImageElement>): HTMLImageElement[] {
   return input.map(item => {
@@ -13,27 +15,49 @@ function normalizeInput(input: Array<string | HTMLImageElement>): HTMLImageEleme
   });
 }
 
-function waitForLoad(img: HTMLImageElement): Promise<ReturnItem> {
-  return new Promise((resolve) => {
-    if (!img.complete) {
-      img.onload = () => resolve([img, 'ok']);
-      return;
-    }
+function isNotStarted(img: HTMLImageElement): boolean {
+  return img.src === '' && img.complete;
+}
 
-    if (img.complete && img.naturalWidth === 0) {
-      resolve([img, 'fail']);
-      return;
-    }
+function isLoaded(img: HTMLImageElement): boolean {
+  return img.src !== '' && img.complete && img.naturalWidth !== 0 && img.naturalHeight !== 0;
+}
 
-    resolve([img, 'ok']);
+function isFailed(img: HTMLImageElement): boolean {
+  return img.src !== '' && img.complete && img.naturalWidth === 0 && img.naturalHeight === 0;
+}
+
+function isLoading(img: HTMLImageElement): boolean {
+  return img.src !== '' && !img.complete;
+}
+
+async function waitForReady(img: HTMLImageElement): Promise<ReturnItem> {
+  return new Promise(resolve => {
+    if (isNotStarted(img) || isLoading(img)) {
+      img.addEventListener('load', () => resolve([img, 'loaded']));
+      img.addEventListener('error', () => resolve([img, 'failed']));
+    } else if (isLoaded(img)) {
+      resolve([img, 'loaded']);
+    } else if (isFailed(img)) {
+      resolve([img, 'failed']);
+    } else {
+      resolve([img, 'failed']);
+    }
   });
 }
 
-function loadImages(input: Array<string | HTMLImageElement>): Promise<ReturnItem[]> {
+async function loadImages(input: Array<string | HTMLImageElement>): Promise<ReturnItem[]> {
   const images = normalizeInput(input);
-  const promises = images.map(waitForLoad);
+  const promises = images.map(waitForReady);
 
-  return Promise.all(promises);
+  const returnItems = await Promise.all(promises);
+  const anyFailed = returnItems.some(item => item[1] === 'failed');
+
+  if(anyFailed) {
+    throw returnItems;
+  } else {
+    return returnItems;
+  }
 }
 
 module.exports = loadImages;
