912e9890c72f63f6ae515ef45be87f4b014f0281
Александр Гренишин <nd0ut.me@gmail.com>
2018-03-05 01:07:09 +0300

    try to fix chrome sandbox error on travis

--- karma.conf.js
+++ karma.conf.js
@@ -7,11 +7,11 @@ process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 module.exports = function(config) {
   config.set({
-    browsers: ['ChromeHeadlessNoSandbox'],
+    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox'],
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox --disable-setuid-sandbox']
+        flags: ['--no-sandbox']
       }
     },
     plugins: [
