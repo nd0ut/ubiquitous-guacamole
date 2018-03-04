1b446b91171da3b07642eb00323663c94d1cee9a
Александр Гренишин <nd0ut.me@gmail.com>
2018-03-04 18:54:20 +0300

    make tests work with local jsdom

--- test/loadImages.test.js
+++ test/loadImages.test.js
@@ -1,47 +1,24 @@
 const loadImages = require('../src/loadImages');
 const jsdom = require('jsdom');
-const { JSDOM } = jsdom;
+const inputFactory = require('./inputFactory');
 
-const dom = new JSDOM('<!DOCTYPE html>', {
-  resources: "usable"
+const virtualConsole = new jsdom.VirtualConsole();
+jest.setTimeout(10000);
+
+const dom = new jsdom.JSDOM('', {
+  resources: 'usable',
+  virtualConsole
 });
 global.Image = dom.window.Image;
 
-test('should return promise with some result', async () => {
-  const result = await loadImages(['https://static.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg']);
-  expect(result).toBeTruthy();
+it('should be resolved when all images are loaded', async () => {
+  expect.assertions(1);
+  const input = [inputFactory.validUrl()];
+  await expect(loadImages(input)).resolves.toBeTruthy();
 });
 
-// test('should be resolved when all images are loaded', async () => {
-//   const input = [...validUrls, ...validUrls.map(url => createImage(url, 0))];
-//   await expect(loadImages(input)).resolves.toBe();
-// });
-
-// test('should be rejected when at least one image is not loaded', async () => {
-//   const input = [
-//     ...invalidUrls,
-//     ...validUrls,
-//     ...validUrls.map(url => createImage(url, 0)),
-//     ...invalidUrls.map(url => createImage(url, 0)),
-//   ];
-
-//   await expect(loadImages(input)).rejects.toBe();
-// });
-
-// test('should return array of HTMLImageElement with it\'s state', async () => {
-//   const input = [...validUrls, ...validUrls.map(url => createImage(url, 0))];
-
-//   const result = await loadImages(input);
-
-//   expect.assertions(1 + input.length * 3);
-
-//   expect(Array.isArray(result)).toBeTruthy();
-
-//   result.forEach((item) => {
-//     expect(item.length).toEqual(2);
-
-//     const [image, state] = item;
-//     expect(image.src).toBeTruthy(); // check if it is HTMLImageElement
-//     expect(typeof state).toEqual('string');
-//   });
-// });
+it('should be rejected when at least one image is not loaded', async () => {
+  expect.assertions(1);
+  const input = [inputFactory.validUrl(), inputFactory.invalidImg()];
+  await expect(loadImages(input)).rejects.toBeTruthy();
+});
