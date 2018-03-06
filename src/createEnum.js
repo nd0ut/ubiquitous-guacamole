1104f7de12e09392536183865e2f6c859f921585
Александр Гренишин <nd0ut.me@gmail.com>
2018-03-06 11:13:05 +0300

    lint

--- src/createEnum.js
+++ src/createEnum.js
@@ -2,11 +2,11 @@ export default function createEnum<T: {}>(map: T): T {
   return new Proxy(map, {
     set: () => false,
     get: (target, name) => {
-      if(target[name]) {
+      if (target[name]) {
         return target[name];
       }
 
       throw new Error(`Property '${name}' not found.`);
     }
   });
-};
+}
