d6d727555a45f82eb5c4c078413a226fc249977f
Александр Гренишин <nd0ut.me@gmail.com>
2018-03-06 11:12:55 +0300

    remove browser field from package.json

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,3 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
 import flow from 'rollup-plugin-flow';
 import pkg from './package.json';
 
@@ -8,11 +6,8 @@ export default [
     input: 'src/index.js',
     plugins: [
       flow({ pretty: true, all: true }),
-      resolve(),
-      commonjs()
     ],
     output: [
-      { name: 'ubiquitous-guacamole', file: pkg.browser, format: 'umd', strict: true },
       { file: pkg.main, format: 'cjs' },
       { file: pkg.module, format: 'es' }
     ]
